4eaffe9a5c273c3018057fb9aec26d61c8a6af28
Nadir Özkan <nadir.ozkan@likom.com.tr>
2019-12-09 14:34:56 +0300

    Navbar bileşenin içi boşaltıldı

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Navbar from './Navbar.jsx';
 
 class Main extends React.Component {
   render(){
--- app/components/pages/Navbar.jsx
+++ app/components/pages/Navbar.jsx
@@ -1,40 +1,10 @@
 import React from 'react';
-import {Link, IndexLink} from 'react-router';
 
 class Navbar extends React.Component{
-
-  onSearch(event){
-    event.preventDefault();
-    let searchText = this.refs.navSearch.value;
-    // Önemli
-    // Url olarak kullanman gereken bir metin var ise
-    // mutlaka encodeURIComponent kullanarak kodla
-    // böylece url içinde kullanılmaya uygun hale gelir...
-    searchText = encodeURIComponent(searchText);
-
-    if (searchText.length > 0 ){
-      this.refs.navSearch.value = '';
-      window.location.hash = '#/?city='+searchText;
-    }
-  }
 
   render(){
     return(
       <div className="top-bar">
-        <div className="top-bar-left">
-          <ul className="menu">
-            <li className="menu-text">React Hava Durumu</li>
-            <li>
-              <IndexLink to="/" activeClassName="activeLink">Hava Durumu</IndexLink>
-            </li>
-            <li>
-              <Link to="/about" activeClassName="activeLink">Hakkında</Link>
-            </li>
-            <li>
-              <Link to="/examples" activeClassName="activeLink">Örnekler</Link>
-            </li>
-          </ul>
-        </div>
       </div>
     );
   }
