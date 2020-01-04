6ad4c34a930f13848ec66639c944c7c51c425ccc
Nadir Özkan <nadir.ozkan@likom.com.tr>
2020-01-04 22:28:02 +0300

    DebugMode => false

--- app/App.js
+++ app/App.js
@@ -15,7 +15,7 @@ class App extends Component {
         this.user = null;
         this.state = {
             hasUser : false,
-            debugMode : true
+            debugMode : false
         }
     }
 
